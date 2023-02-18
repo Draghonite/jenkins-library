5240b44555be3153536f9b1f99d9b273b7cc9919
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-17 21:42:08 -0500

    Update pushArtifactory.groovy

--- vars/pushArtifactory.groovy
+++ vars/pushArtifactory.groovy
@@ -1,5 +1,5 @@
-def call(String ARTIFACTORY_CREDS_ID, ARTIFACTORY_SERVER, String PACKAGE_PATH, String PACKAGE_NAME, 
-         String BUILD_ENV, String BUILD_PREFIX, String BUILD_NUMBER, String REPO="libs-release-local") {
+def call(ARTIFACTORY_CREDS_ID, ARTIFACTORY_SERVER, PACKAGE_PATH, PACKAGE_NAME, 
+        BUILD_ENV, BUILD_PREFIX, BUILD_NUMBER, REPO="libs-release-local") {
     withCredentials([usernameColonPassword(credentialsId: $ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh '''
             apk add curl

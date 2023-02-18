d615f47b51574b68a4cdc77c529b6615e84e84cc
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-18 18:48:31 -0500

    Added sonarscan function

---
+++ vars/sonarScan.groovy
@@ -0,0 +1,5 @@
+def call(Map params) {
+    withSonarQubeEnv(params.SONAR_SERVER_ID) {
+        sh "cd ${params.SONAR_PROJECT_BASE_PATH} && sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.projectKey=${params.SONAR_PROJECT_KEY}"
+    }
+}

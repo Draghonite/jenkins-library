f85066d5d8b48b786076f38f898a7ce51c3c1629
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-17 23:52:55 -0500

    Update pushArtifactory.groovy

--- vars/pushArtifactory.groovy
+++ vars/pushArtifactory.groovy
@@ -1,10 +1,11 @@
 def call(Map params) {
-    withCredentials([usernameColonPassword(credentialsId: $params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
-        sh '''
-            apk add curl
-            curl -i -u "$params.ARTIFACTORY_USER_PASS" \
-                -T $params.PACKAGE_PATH \
-                "$params.ARTIFACTORY_SERVER/$params.REPO/$params.PACKAGE_NAME-$params.BUILD_ENV/$params.PACKAGE_NAME-$params.BUILD_ENV-$params.BUILD_PREFIX.$params.BUILD_NUMBER.zip"
-        '''
-    }
+    echo "pushArtifactory called! ${params.ARTIFACTORY_CREDS_ID} ${params.ARTIFACTORY_SERVER}"
+    // withCredentials([usernameColonPassword(credentialsId: $params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
+    //     sh '''
+    //         apk add curl
+    //         curl -i -u "$params.ARTIFACTORY_USER_PASS" \
+    //             -T $params.PACKAGE_PATH \
+    //             "$params.ARTIFACTORY_SERVER/$params.REPO/$params.PACKAGE_NAME-$params.BUILD_ENV/$params.PACKAGE_NAME-$params.BUILD_ENV-$params.BUILD_PREFIX.$params.BUILD_NUMBER.zip"
+    //     '''
+    // }
 }

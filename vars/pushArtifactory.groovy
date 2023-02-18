8f5d4774fcddb02c51ae908fa54ff3d8ed2b7893
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-18 14:58:20 -0500

    Update pushArtifactory.groovy

--- vars/pushArtifactory.groovy
+++ vars/pushArtifactory.groovy
@@ -1,13 +1,5 @@
 def call(Map params) {
-    sh "echo ${params.BUILD_ENV}"
-//     echo "pushArtifactory called! $params ${params} ${params.BUILD_ENV} params.ARTIFACTORY_SERVER"
-//     sh ''' echo "pushArtifactory called! $params ${params} params.ARTIFACTORY_CREDS_ID params.ARTIFACTORY_SERVER" '''
-    // withCredentials([usernameColonPassword(credentialsId: $params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
-    //     sh '''
-    //         apk add curl
-    //         curl -i -u "$params.ARTIFACTORY_USER_PASS" \
-    //             -T $params.PACKAGE_PATH \
-    //             "$params.ARTIFACTORY_SERVER/$params.REPO/$params.PACKAGE_NAME-$params.BUILD_ENV/$params.PACKAGE_NAME-$params.BUILD_ENV-$params.BUILD_PREFIX.$params.BUILD_NUMBER.zip"
-    //     '''
-    // }
+    withCredentials([usernameColonPassword(credentialsId: $params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
+        sh "apk add curl && curl -i -u ${params.ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_PATH} ${params.ARTIFACTORY_SERVER}/${params.REPO}/${params.PACKAGE_NAME}-${params.BUILD_ENV}/${params.PACKAGE_NAME}-${params.BUILD_ENV}-${params.BUILD_PREFIX}.${params.BUILD_NUMBER.zip}"
+    }
 }

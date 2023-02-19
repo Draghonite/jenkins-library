0f32b825d44564c6c2eb95f32883cd63a33c3380
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 16:10:35 -0500

    wip

--- vars/archiveArtifact.groovy
+++ vars/archiveArtifact.groovy
@@ -4,6 +4,7 @@ def call(Map params) {
         // sh "ARTIFACT_MD5_CHECKSUM = \$(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
         // sh "ARTIFACT_SHA1_CHECKSUM = \$(sha1sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
         // sh "ARTIFACT_SHA256_CHECKSUM = \$(sha256sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
-        sh "ARTIFACT_MD5_CHECKSUM=\$(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }' && echo \$ARTIFACT_MD5_CHECKSUM && curl -H \"X-Checksum-MD5:\$ARTIFACT_MD5_CHECKSUM\" -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+        sh "ARTIFACT_MD5_CHECKSUM=\$(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }' && echo \$ARTIFACT_MD5_CHECKSUM"
+        sh "curl -H \"X-Checksum-MD5:\$ARTIFACT_MD5_CHECKSUM\" -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
     }
 }

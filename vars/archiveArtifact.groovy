e724fa3a8bbf24f8316ff11cdd87192eedaff201
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 15:12:31 -0500

    Removed posting checksum to let artifactory calculate it

--- vars/archiveArtifact.groovy
+++ vars/archiveArtifact.groovy
@@ -1,9 +1,6 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh "apk add curl"
-        // sh "ARTIFACT_MD5_CHECKSUM = \$(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
-        // sh "ARTIFACT_SHA1_CHECKSUM = \$(sha1sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
-        // sh "ARTIFACT_SHA256_CHECKSUM = \$(sha256sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
-        sh "ARTIFACT_MD5_CHECKSUM=\$(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }' && curl -H \"X-Checksum-MD5:12345\" -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+        sh "curl -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
     }
 }

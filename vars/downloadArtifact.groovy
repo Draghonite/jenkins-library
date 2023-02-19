f1b42f0dc083e5235afa0d4d4e6484a728478624
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 18:23:47 -0500

    wip

--- vars/downloadArtifact.groovy
+++ vars/downloadArtifact.groovy
@@ -1,6 +1,6 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh "apk add curl"
-        sh "cd ${params.PACKAGES_PATH} && curl -iu ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+        sh "cd ${params.PACKAGES_PATH} && curl -u ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
     }
 }

157ff04448424bd6592204a3d3612c44dda3891e
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-18 23:58:23 -0500

    Added md5, sha1 and sha256 checksum on artifac upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jenkins-library
 Utility library for Jenkins Pipelines
+
+### Credits:
+- https://stackoverflow.com/questions/40009688/artifactory-upload-with-checksum
--- vars/archiveArtifact.groovy
+++ vars/archiveArtifact.groovy
@@ -1,5 +1,9 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
-        sh "apk add curl && curl -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+        sh "apk add curl"
+        sh "ARTIFACT_MD5_CHECKSUM = $(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print $1 }'"
+        sh "ARTIFACT_SHA1_CHECKSUM = $(sha1sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print $1 }'"
+        sh "ARTIFACT_SHA256_CHECKSUM = $(sha256sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print $1 }'"
+        sh "curl --H \"X-Checksum-MD5:${ARTIFACT_MD5_CHECKSUM}\" --H "X-Checksum-Sha1:${ARTIFACT_SHA1_CHECKSUM}" --H "X-Checksum-Sha256:${ARTIFACT_SHA256_CHECKSUM}" -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
     }
 }

865c0e9ddfca0fb3ad6d15240bb1707588173b4a
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-18 21:51:28 -0500

    Added downloadartifact function

---
+++ vars/downloadArtifact.groovy
@@ -0,0 +1,5 @@
+def call(Map params) {
+    withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
+        sh "apk add curl && cd ${params.PACKAGES_PATH} && curl -i -u ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+    }
+}

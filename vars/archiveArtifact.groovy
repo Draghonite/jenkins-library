be414d3cd8267cfd0779ebaf03095703cdcf8c91
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 00:02:58 -0500

    Added md5, sha1 and sha256 checksum on artifac upload

--- vars/archiveArtifact.groovy
+++ vars/archiveArtifact.groovy
@@ -4,6 +4,6 @@ def call(Map params) {
         sh "ARTIFACT_MD5_CHECKSUM = $(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print $1 }'"
         sh "ARTIFACT_SHA1_CHECKSUM = $(sha1sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print $1 }'"
         sh "ARTIFACT_SHA256_CHECKSUM = $(sha256sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print $1 }'"
-        sh "curl --H \"X-Checksum-MD5:${ARTIFACT_MD5_CHECKSUM}\" --H "X-Checksum-Sha1:${ARTIFACT_SHA1_CHECKSUM}" --H "X-Checksum-Sha256:${ARTIFACT_SHA256_CHECKSUM}" -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+        sh "curl --H \"X-Checksum-MD5:${ARTIFACT_MD5_CHECKSUM}\" --H \"X-Checksum-Sha1:${ARTIFACT_SHA1_CHECKSUM}\" --H \"X-Checksum-Sha256:${ARTIFACT_SHA256_CHECKSUM}\" -i -u ${ARTIFACTORY_USER_PASS} -T ${params.PACKAGE_BUILD_PATH} ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
     }
 }

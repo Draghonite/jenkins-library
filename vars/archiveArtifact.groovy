1ed020ba12a5d45ee825ba16e7c543b8a84f8b17
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 16:53:09 -0500

    wip

--- vars/archiveArtifact.groovy
+++ vars/archiveArtifact.groovy
@@ -1,7 +1,7 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh "apk add curl"
-        sh "ARTIFACT_MD5_CHECKSUM=\$(md5sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
+        sh "ARTIFACT_MD5_CHECKSUM=md5sum ${params.PACKAGE_BUILD_PATH} | awk '{ print \$1 }'"
         sh "ARTIFACT_SHA1_CHECKSUM=\$(sha1sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
         sh "ARTIFACT_SHA256_CHECKSUM=\$(sha256sum ${params.PACKAGE_BUILD_PATH}) | awk '{ print \$1 }'"
         // TODO: revisit checksum headers -- may not be properly replaced in curl command

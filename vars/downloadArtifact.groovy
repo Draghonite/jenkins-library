2ead92bd04588e92aabef80c6cc6d4e3f1b89a76
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 22:24:14 -0500

    wip

--- vars/downloadArtifact.groovy
+++ vars/downloadArtifact.groovy
@@ -4,7 +4,7 @@ def call(Map params) {
         sh "mkdir -p ${params.PACKAGES_PATH}"
         sh "cd ${params.PACKAGES_PATH} && curl -u ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
         dir("${params.PACKAGES_PATH}") {
-            stash(name: 'myenv', includes: '**')
+            stash(name: 'artifactstash')
         }
     }
 }

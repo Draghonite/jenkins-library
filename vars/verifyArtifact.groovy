7362cd2ea06cc1e4cbe6c96d42b9e520b731ddf9
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 21:00:14 -0500

    wip

--- vars/verifyArtifact.groovy
+++ vars/verifyArtifact.groovy
@@ -3,7 +3,7 @@ def call(Map params) {
         sh "apk add curl jq"
         sh "mkdir -p ${params.PACKAGES_PATH}"
         dir("${params.PACKAGES_PATH}") {
-            unstash name: "${params.PACKAGE_NAME}"
+            unstash "${params.PACKAGE_NAME}"
         }
         sh "cd ${params.PACKAGES_PATH} && md5sum ${params.PACKAGE_NAME} > sum1.txt && curl -su ${ARTIFACTORY_USER_PASS} ${params.ARTIFACTORY_SERVER}/api/storage/libs-release-local/${params.PACKAGE_REPO_PATH} | jq -r '.checksums.md5 +\"  ${params.PACKAGE_NAME}\"' > sum2.txt && md5sum -c sum1.txt sum2.txt"
     }

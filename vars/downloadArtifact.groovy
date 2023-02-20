6994937c00c7f1c9c51598cadd5e5eb8d60bd722
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 23:36:27 -0500

    wip

--- vars/downloadArtifact.groovy
+++ vars/downloadArtifact.groovy
@@ -1,11 +1,11 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh "apk add curl"
-        sh "mkdir -p ${params.PACKAGES_PATH}"
-        sh "cd ${params.PACKAGES_PATH} && curl -u ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
-        // dir('/packages') {
-        //     stash(name: 'artifactstash', includes: '/packages/.')
-        // }
-        stash name: 'artifactstash', includes: '/packages/reactivities-client-dev-1.0.226.tar.gz'
+        sh "mkdir -p ./packages"
+        sh "cd ./packages && curl -u ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+        dir('./packages') {
+            stash(name: 'artifactstash')
+        }
+        // stash name: 'artifactstash', includes: '/packages/reactivities-client-dev-1.0.226.tar.gz'
     }
 }

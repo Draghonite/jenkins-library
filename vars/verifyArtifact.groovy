840e4ba38d80e4d362d72c5dc758adb7084c9598
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 19:29:01 -0500

    wip

--- vars/verifyArtifact.groovy
+++ vars/verifyArtifact.groovy
@@ -1,6 +1,8 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh "apk add curl jq"
-        sh "cd ${params.PACKAGES_PATH} && curl -isu ${ARTIFACTORY_USER_PASS} ${params.ARTIFACTORY_SERVER}/api/storage/libs-release-local/${params.PACKAGE_REPO_PATH} | jq -r '.checksums.md5' > sum2.txt"
+        // TODO: resume here -- download the report checksum and compare with current checksum -- acquire path to the downloaded artifact from params
+        sh "cd ${params.PACKAGES_PATH} && md5sum ${params.PACKAGE_NAME} > sum1.txt && curl -su ${ARTIFACTORY_USER_PASS} ${params.ARTIFACTORY_SERVER}/api/storage/libs-release-local/${params.PACKAGE_REPO_PATH} | jq -r '.checksums.md5' > sum2.txt"
+        input message:'verify file'
     }
 }

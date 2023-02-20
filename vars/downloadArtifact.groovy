160b7cdc4119df0c36de932652de108799e8d10e
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-19 23:50:08 -0500

    wip

--- vars/downloadArtifact.groovy
+++ vars/downloadArtifact.groovy
@@ -1,11 +1,10 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh "apk add curl"
-        sh "mkdir -p ./packages"
-        sh "cd ./packages && curl -u ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
-        dir('./packages') {
+        sh "mkdir -p ./artifacts"
+        sh "cd ./artifacts && curl -u ${ARTIFACTORY_USER_PASS} -O ${params.ARTIFACTORY_SERVER}/libs-release-local/${params.PACKAGE_REPO_PATH}"
+        dir('./artifacts') {
             stash(name: 'artifactstash')
         }
-        // stash name: 'artifactstash', includes: '/packages/reactivities-client-dev-1.0.226.tar.gz'
     }
 }
--- vars/verifyArtifact.groovy
+++ vars/verifyArtifact.groovy
@@ -1,11 +1,10 @@
 def call(Map params) {
     withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
         sh "apk add curl jq"
-        sh "mkdir -p ./packages"
-        dir('./packages') {
+        sh "mkdir -p ./artifacts"
+        dir('./artifacts') {
             unstash 'artifactstash'
         }
-        sh "ls ./packages"
-        sh "cd ./packages && md5sum ${params.PACKAGE_NAME} > sum1.txt && curl -su ${ARTIFACTORY_USER_PASS} ${params.ARTIFACTORY_SERVER}/api/storage/libs-release-local/${params.PACKAGE_REPO_PATH} | jq -r '.checksums.md5 +\"  ${params.PACKAGE_NAME}\"' > sum2.txt && md5sum -c sum1.txt sum2.txt"
+        sh "cd ./artifacts && md5sum ${params.PACKAGE_NAME} > sum1.txt && curl -su ${ARTIFACTORY_USER_PASS} ${params.ARTIFACTORY_SERVER}/api/storage/libs-release-local/${params.PACKAGE_REPO_PATH} | jq -r '.checksums.md5 +\"  ${params.PACKAGE_NAME}\"' > sum2.txt && md5sum -c sum1.txt sum2.txt"
     }
 }

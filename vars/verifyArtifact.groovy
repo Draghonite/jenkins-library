095f481769ad579b06d6a494b39e9abfac3d9752
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-21 19:15:53 -0500

    wip

--- vars/verifyArtifact.groovy
+++ vars/verifyArtifact.groovy
@@ -6,8 +6,5 @@ def call(Map params) {
             unstash 'artifactstash'
         }
         sh "cd ./artifacts && md5sum ${params.PACKAGE_NAME} > sum1.txt && curl -su ${ARTIFACTORY_USER_PASS} ${params.ARTIFACTORY_SERVER}/api/storage/libs-release-local/${params.PACKAGE_REPO_PATH} | jq -r '.checksums.md5 +\"  ${params.PACKAGE_NAME}\"' > sum2.txt && md5sum -c sum1.txt sum2.txt"
-        dir('./artifacts') {
-            stash(name: 'artifactstash')
-        }
     }
 }

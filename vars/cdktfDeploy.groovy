579ab7ca96ead21998d581160c338ed683d110ee
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-22 00:45:25 -0500

    wip

--- vars/cdktfDeploy.groovy
+++ vars/cdktfDeploy.groovy
@@ -1,21 +1,25 @@
 def call(Map params) {
-    sh '''
-        apk update && apk add terraform
-        npm install -y -g cdktf-cli
-        npm install -y -g typescript
-        npm install
-        rm -rf ./artifacts && mkdir ./artifacts
-    '''
-    dir('./artifacts') {
-        unstash 'artifactstash'
+    withCredentials([usernameColonPassword(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID')]) {
+        withCredentials([usernameColonPassword(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
+            sh '''
+                apk update && apk add terraform
+                npm install -y -g cdktf-cli
+                npm install -y -g typescript
+                npm install
+                rm -rf ./artifacts && mkdir ./artifacts
+            '''
+            dir('./artifacts') {
+                unstash 'artifactstash'
+            }
+            sh '''
+                cd ./artifacts
+                mkdir ./release && cd ./release
+                tar -xzvf ../$PACKAGE_NAME .
+                cdktf deploy --auto-deploy
+            '''
+            // TODO: fix 'cdktf deploy' results in ERROR Raw mode is not supported on the current process.stdin
+            sh "echo TODO: Deploy the ${BUILD_ENV} build to ${DEPLOY_ENV}."
+            sh "./artifacts"
+        }
     }
-    sh '''
-        cd ./artifacts
-        mkdir ./release && cd ./release
-        tar -xzvf ../$PACKAGE_NAME .
-        cdktf deploy --auto-deploy
-    '''
-    // TODO: fix 'cdktf deploy' results in ERROR Raw mode is not supported on the current process.stdin
-    sh "echo TODO: Deploy the ${BUILD_ENV} build to ${DEPLOY_ENV}."
-    sh "./artifacts"
 }

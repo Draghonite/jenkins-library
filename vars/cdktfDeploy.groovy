8843d6eb043a1ebf9275f7aa6cc3e02697abbbb8
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-22 21:57:21 -0500

    wip

--- vars/cdktfDeploy.groovy
+++ vars/cdktfDeploy.groovy
@@ -1,7 +1,16 @@
 def call(Map params) {
-    // TODO: also need a way to change the environment (AWS Account) based on params.DEPLOY_ENV -- 
-    withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID')]) {
-        withCredentials([string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
+    withCredentials([string(credentialsId: "AWS_ACCESS_KEY_ID_${params.DEPLOY_ENV}", variable: 'AWS_ACCESS_KEY_ID')]) {
+        withCredentials([string(credentialsId: "AWS_SECRET_ACCESS_KEY_${params.DEPLOY_ENV}", variable: 'AWS_SECRET_ACCESS_KEY')]) {
+            def AWS_AVAILABILITY_ZONE = "${params.AWS_REGION}a";
+            def AWS_BUNDLE_ID = "nano_2_0";
+            switch (params.DEPLOY_ENV) {
+                case "qa":
+                    AWS_BUNDLE_ID = "small_2_0"
+                    break;
+                // case "prod":
+                //     AWS_BUNDLE_ID = "medium_2_0"
+                //     break;
+            }
             sh '''
                 apk update && apk add terraform
                 npm install -y -g cdktf-cli
@@ -12,12 +21,11 @@ def call(Map params) {
             dir('./artifacts') {
                 unstash 'artifactstash'
             }
-            // TODO: fix 'cdktf deploy' results in ERROR Raw mode is not supported on the current process.stdin
             sh """
                 cd ./artifacts
                 rm -rf ./release && mkdir ./release && cd ./release
                 tar -xzvf ../${params.PACKAGE_NAME} .
-                cdktf deploy --auto-approve
+                AWS_REGION=${params.AWS_REGION} AWS_AVAILABILITY_ZONE=${AWS_AVAILABILITY_ZONE} AWS_BUNDLE_ID=${AWS_BUNDLE_ID} DEPLOY_ENV=${params.DEPLOY_ENV} cdktf deploy --auto-approve
                 echo Deployed the ${params.BUILD_ENV} build to ${params.DEPLOY_ENV}.
             """
         }

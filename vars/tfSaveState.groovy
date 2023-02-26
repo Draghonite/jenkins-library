93a9c6e29bfe13f013c27cb73909b35eeed7d925
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-02-26 18:28:13 -0500

    wip

--- vars/tfSaveState.groovy
+++ vars/tfSaveState.groovy
@@ -1,7 +1,12 @@
 def call(Map params) {
     withCredentials([string(credentialsId: "AWS_ACCESS_KEY_ID_${params.DEPLOY_ENV}", variable: 'AWS_ACCESS_KEY_ID')]) {
         withCredentials([string(credentialsId: "AWS_SECRET_ACCESS_KEY_${params.DEPLOY_ENV}", variable: 'AWS_SECRET_ACCESS_KEY')]) {
-            input message="TODO: receive path to the terraform project and POST the terraform.tfstate file to S3"
+            sh """
+                apk update && apk add terraform aws-cli
+                cd ${params.TF_PROJECT_PATH}
+                echo Saving Terraform state remotely
+                AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} AWS_DEFAULT_REGION=${params.AWS_REGION} aws s3 cp terraform.tfstate ${params.TF_STATE_S3_BUCKET_URL}
+            """
         }
     }
 }

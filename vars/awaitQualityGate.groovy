daeb138b1d14e911b6ce71b68fdb465ba71ddd8a
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-03-19 13:57:58 -0400

    Update awaitQualityGate.groovy

--- vars/awaitQualityGate.groovy
+++ vars/awaitQualityGate.groovy
@@ -1,8 +1,6 @@
 def call(Map params) {
     sleep(60)
     timeout(time: 1, unit: 'HOURS') {
-            withCredentials([usernameColonPassword(credentialsId: params.ARTIFACTORY_CREDS_ID, variable: 'ARTIFACTORY_USER_PASS')]) {
-
         waitForQualityGate abortPipeline: true
     }
 }

427e44cd5b2db59dc79b38b1f52968c0a662d6c5
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2023-03-24 21:22:33 -0400

    added sonar exclusions

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,5 +1,5 @@
 def call(Map params) {
     withSonarQubeEnv(params.SONAR_SERVER_ID) {
-        sh "cd ${params.SONAR_PROJECT_BASE_PATH} && sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.projectKey=${params.SONAR_PROJECT_KEY}"
+        sh "cd ${params.SONAR_PROJECT_BASE_PATH} && sonar-scanner -Dsonar.host.url=$SONAR_HOST_URL -Dsonar.login=$SONAR_AUTH_TOKEN -Dsonar.projectKey=${params.SONAR_PROJECT_KEY} -Dsonar.exclusions=${params.SONAR_EXCLUSIONS}"
     }
 }
